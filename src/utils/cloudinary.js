86c7b5f98173a5cbdce580fd13de56f3f1f12a84
surajrathor997 <surajrathor8989@gmail.com>
2025-07-14 08:40:55 +0530

    file path config and setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,52 @@
+import {v2 as cloudinary} from "cloudinary"
+
+import fs from "fs"
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+
+
+
+const uploadOnCloudinary = async (localFilePath) =>{
+    try{
+        if(!localFilePath){
+            console.log("file not found");
+            return null
+        }
+        const response = await cloudinary.uploader.upload(localFilePath,{resource_type:"auto"})
+
+        // File hasbeen uploaded successfully
+        console.log("File is uploaded on cloudinary",response.url);
+        return response;
+
+
+
+    }
+    catch(error){
+        fs.unlinkSync(localFilePath) // remove the locally saved temparary file as the upload operation got failed
+        return null;
+
+    }
+}
+
+ 
+
+
+export {uploadOnCloudinary}
+
+
+
+
+
+
+
+
+
+
+
+ 
